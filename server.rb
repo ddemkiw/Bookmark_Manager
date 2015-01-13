bf4a8e2c63210be9d72f60474d32eaf960085e48
bibiana <bcristofol@gmail.com>
2015-01-13 14:40:16 +0000

    Adding links when browsing the homepage

--- server.rb
+++ server.rb
@@ -15,3 +15,10 @@ DataMapper.auto_upgrade!
     @links = Link.all
     erb :index
   end 
+
+  post '/links' do
+  	url = params["url"]
+  	title = params["title"]
+  	Link.create(:url => url, :title => title)
+  	redirect to ('/')
+  end
