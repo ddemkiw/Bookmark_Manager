b1454abbaa0b489d2b84755b1416fe220dfe6f86
Danielle Demkiw <ddemkiw@gmail.com>
2015-01-14 12:35:42 +0000

    MERGING

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 feature "User signs up" do
 
 
-  scenario "when being logged out" do    
-    lambda { sign_up }.should change(User, :count).by(1)    
+ scenario "when being a new user visiting the site" do
+    expect{ sign_up }.to change(User, :count).by(1)
     expect(page).to have_content("Welcome, alice@example.com")
-    expect(User.first.email).to eq("alice@example.com")        
+    expect(User.first.email).to eq("alice@example.com")
   end
 
   scenario "with a password that doesn't match" do
-    lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0) 
-    expect(current_path).to eq('/users')   
+    expect{ sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+    expect(current_path).to eq('/users')
     expect(page).to have_content("Sorry, your passwords don't match")
   end
 
-  scenario "with an email that is already registered" do    
-    lambda { sign_up }.should change(User, :count).by(1)
-    lambda { sign_up }.should change(User, :count).by(0)
+  scenario "with an email that is already registered" do
+    expect{ sign_up }.to change(User, :count).by(1)
+    expect{ sign_up }.to change(User, :count).by(0)
     expect(page).to have_content("This email is already taken")
   end
 
